1b2531a0bd98a29fea94b6082793b0a617e1acf0
aditya <araj59197@gmail.com>
2023-01-12 20:43:19 +0530

    first commit

---
+++ src/components/GameBoard.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import '../Game.css';
+
+import Header from "./Header";
+import Footer from "./Footer";
+import GameCircle from "./GameCircle";
+
+const NO_CIRCLES = 16;
+const NO_PLAYER = 0;
+const Player_1 = 1;
+const Player_2 = 2;
+
+const GameBoard = () => {
+    const [gameBoard, setGameBoard] = useState(Array(16).fill(NO_PLAYER));
+    const [currentPlayer, setCurrentPlayer] = useState(Player_1);
+
+    console.log(gameBoard);
+
+    const initBoard = () => {
+        const circles = [];
+        for (let i = 0; i < NO_CIRCLES; i++) {
+            circles.push(renderCircle(i));
+        }
+        return circles;
+    }
+
+    const circleClicked = (id) => {
+        console.log("circle clicked:" + id);
+
+        setGameBoard(prev => {
+            return prev.map((circle, pos) => {
+                if (pos === id)
+                    return currentPlayer;
+                return circle;
+            })
+        })
+
+        setCurrentPlayer(currentPlayer === Player_1 ? Player_2 : Player_1);
+
+        console.log(gameBoard);
+        console.log(currentPlayer);
+    }
+
+    const renderCircle = id => {
+        return <GameCircle key={id} id={id} className={`player_${gameBoard[id]}`} onCircleClicked={circleClicked} />
+    }
+
+    return (
+        <>
+            <Header/>
+            <div className="gameBoard">
+                {initBoard()}
+            </div>
+             <Footer/> 
+        </>
+    )
+}
+
+export default GameBoard;
